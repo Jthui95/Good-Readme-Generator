7b696903d2743b840b3ae06a16c643ead5943137
Jthui95 <jonathanhui.hk@gmail.com>
2020-08-12 13:22:51 -0700

    added inputs with questions. Last push I just added the develop folder.

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,60 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require('fs');
 
-];
+const questions = inquirer.prompt([
+{
+    type: 'input',
+    message: 'What would you like to name your read me file?',
+    name:'Title',
+
+
+},
+{
+    type: 'input',
+    message: 'Please Write a Description for your read me file.',
+    name: 'description', 
+},
+{
+    type: 'input',
+    message:"Please list out your table of contents.",
+    name: 'contents'
+},
+{
+    type: 'input',
+    message: 'Please tell the user how they would use your application.',
+    name:'usage',
+},
+{
+    type:'input',
+    message:'Please add a license if applicable if not skip this.',
+    name:'license',
+},
+{
+    type:'input',
+    message:'Please tell us whether or not you would like others to contribute to your code. If so what should they do?',
+    name:'contributing',
+},
+{
+    type:'input',
+    message:'',
+    name:'tests',
+},
+{
+    type:'list',
+    message:"Please input your Github profile picture and GitHub Email",
+    name:'questions',
+    choices:[
+        'profile picture',
+        'github email',
+    ] 
+    
+},
+
+]);
 
 function writeToFile(fileName, data) {
+
+    fs.writeFile
 }
 
 function init() {
