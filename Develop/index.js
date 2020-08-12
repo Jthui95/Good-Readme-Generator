a9a002e504406e3afd9d59cff9c365e851439a69
Jthui95 <jonathanhui.hk@gmail.com>
2020-08-12 15:20:46 -0700

    added validators and defaults to some of the inputs

--- Develop/index.js
+++ Develop/index.js
@@ -1,32 +1,46 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
 
-const questions = inquirer.prompt([
+//Inquirer prompts from User Responses
+const questions = [
 {
     type: 'input',
     message: 'What would you like to name your read me file?',
     name:'Title',
-
-
+    default: 'readme.txt',
+    validate: function(answer){
+        if (answer.length < 1) {
+            return console.log('A title for your read me is required.')
+        } else
+        return true;
+    }
 },
 {
     type: 'input',
-    message: 'Please Write a Description for your read me file.',
+    message: 'Please write a description for your  file.',
     name: 'description', 
+    default: 'File Descritption',
+    validate: function(answer) {
+        if (answer.lenght < 1) {
+            return console.log("A description is requried for your read me.")
+        }
+    }
 },
 {
     type: 'input',
     message:"Please list out your table of contents.",
     name: 'contents'
+    
 },
 {
     type: 'input',
     message: 'Please tell the user how they would use your application.',
     name:'usage',
+    
 },
 {
     type:'input',
-    message:'Please add a license if applicable if not skip this.',
+    message:'Please add a license if applicable, if not skip this.',
     name:'license',
 },
 {
@@ -36,25 +50,45 @@ const questions = inquirer.prompt([
 },
 {
     type:'input',
-    message:'',
+    message:'If you have any tests for your application, let us know how to run them. If not skip them.',
     name:'tests',
 },
 {
-    type:'list',
-    message:"Please input your Github profile picture and GitHub Email",
-    name:'questions',
-    choices:[
-        'profile picture',
-        'github email',
-    ] 
-    
+    type: 'input',
+        message: "What is your GitHub username? ",
+        name: 'username',
+        default: 'jthui95',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please enter a valid Github username.");
+            }
+            return true;
+        }
+},
+{
+    type: 'input',
+    message: "What is the name of your GitHub repo?",
+    name: 'repo',
+    default: 'readme-generator',
+    validate: function (answer) {
+        if (answer.length < 1) {
+            return console.log("A valid GitHub repo is required for a badge.");
+        }
+        return true;
+    }
 },
 
-]);
+];
 
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, JSON.stringify(data), function(err){
+       
+        if (err) {
+            return console.log(err);
+        }
 
-    fs.writeFile
+        console.log("Readme.txt complete!")
+    });
 }
 
 function init() {
